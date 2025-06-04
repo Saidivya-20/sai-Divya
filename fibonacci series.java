511dab51949d584119cad5e5bf9fcd55b32615dd
Saidivya-20 <saidivya3652@gmail.com>
2025-06-04 11:19:46 +0530

    Add files via upload

---
+++ fibonacci series.java
@@ -0,0 +1,19 @@
+class fibonacci series{
+  static int fibonacci(int n){
+  if(n==0){
+    return 0;
+	}
+	else if(n==1){
+	return 1;
+  }
+  else{
+	return fibonacci(n-1)+fibonacci(n-2);
+	}
+  }
+  public static void main(String[] args){
+  int number = 5;
+  System.out.println("fibonacci series:");
+  for(int i=0;i<number;i++)
+  System.out.println(fibonacci(i)+"");
+  }
+}  
