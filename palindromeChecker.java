0289307add7c34f7c290b6f7c5386e3c473cd4d1
Saidivya-20 <saidivya3652@gmail.com>
2025-06-05 13:53:09 +0530

    Add files via upload

---
+++ palindromeChecker.java
@@ -0,0 +1,24 @@
+public class PalindromeChecker {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String input = "madam";
+        String processed = input.toLowerCase().replaceAll("[^a-z]", "");
+
+        boolean result = isPalindrome(processed, 0, processed.length() - 1);
+
+        if (result) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+    }
+}
